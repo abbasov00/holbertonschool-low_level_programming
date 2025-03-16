01030c851becd821abc74088fdc6ca705afb5799
abbasov00 <abbaslidadas3@gmail.com>
2025-03-16 19:57:06 +0000

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,17 @@
-/**
- * _strchr - locates a character in a string
- * @s: pointer to string
- * @c: character to locate
- * Return: pointer to first occurence of character c
- * or NULL if character is not found.
- */
+#include "main.h"
 
-char *_strchr(char *s, char c)
+/**
+ * _strlen - main
+ * @s: var 1
+ *
+ * Return: str-length
+ */
+int _strlen(char *s)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		else if (*(s + 1) == c)
-			return (s + 1);
-		s++;
-	}
-	return (s + 1);
+	size_t string_length = 0;
+
+	while (*s++)
+		string_length++;
+
+	return (string_length);
 }
