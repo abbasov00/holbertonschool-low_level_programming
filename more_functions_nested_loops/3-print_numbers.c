7ac0d2d5347d2f61a2f93657a64088882f93b8cd
abbasov00 <abbaslidadas3@gmail.com>
2025-03-16 17:34:21 +0000

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - main
+ * Return: zero
+ */
+void print_numbers(void)
+{
+	int  eded;
+
+	for (eded = 0; eded <= 9; eded++)
+		_putchar(eded + '0');
+	_putchar('\n');
+}
